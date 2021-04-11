476c1d79bc0bcc9271d2bde4c92a2dbffa487260
GDLAB-MSI07\GD Student <996392984@qq.com>
2021-04-11 22:46:32 +0800

    GRAB THING

---
+++ VR_Pro/Assets/ImprovedHandController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class ImprovedHandController : MonoBehaviour
+{
+    [SerializeField] private InputActionReference controllerActionGrip;
+    [SerializeField] private InputActionReference controllerActionTrigger;
+
+    private Animator _handAnimator;
+
+    private void Awake()
+    {
+        controllerActionGrip.action.performed += GripPress;
+        controllerActionTrigger.action.performed += TriggerPress;
+
+        _handAnimator = GetComponent<Animator>();
+    }
+
+    private void TriggerPress(InputAction.CallbackContext obj) =>
+        _handAnimator.SetFloat("Trigger", obj.ReadValue<float>());
+
+    private void GripPress(InputAction.CallbackContext obj) =>
+        _handAnimator.SetFloat("Grip", obj.ReadValue<float>());
+
+
+}
---
+++ VR_Pro/Assets/WonderFood/Scripts/GrabObjectInteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GrabObjectInteraction : MonoBehaviour
+{
+    private XRBaseInteractor interactor;
+    private XRGrabInteractable grabInteractor;
+
+    public void HoverOver()
+    {
+        GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+    }
+
+    private void OnEnable()
+    {
+        grabInteractor = GetComponent<XRGrabInteractable>();
+        
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    public void GrabEnd()
+    {
+        // interactor.GetComponent<XRBaseController>().modelPrefab.gameObject.SetActive(true);
+        //HandModelVisibility(false);
+        //interactor.transform.GetChild(0).gameObject.SetActive(true);
+        interactor.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public  void GrabbedBy()
+    {
+        interactor = GetComponent<XRGrabInteractable>().selectingInteractor;
+        Debug.Log(interactor.name);
+        interactor.transform.GetChild(0).gameObject.SetActive(false);
+
+        //HandModelVisibility(true);
+    }
+
+
+    public void HoverEnd()
+    {
+        GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+    }
+}
