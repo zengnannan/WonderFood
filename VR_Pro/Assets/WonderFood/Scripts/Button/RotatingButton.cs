02e7cd9e9835491e092ed7c721c2f6fd77a789b0
GDLAB-MSI07\GD Student <996392984@qq.com>
2021-04-25 20:44:30 +0800

    Level 4,5,6

--- VR_Pro/Assets/WonderFood/Scripts/Button/RotatingButton.cs
+++ VR_Pro/Assets/WonderFood/Scripts/Button/RotatingButton.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class RotatingButton : MonoBehaviour
 {
     public float RotatingSpeed;
+    public float ChangedScale;
+    public float IdleScale;
+    public float scaleSpeed;
 
     protected virtual void Update()
     {
         transform.Rotate(Vector3.up * Time.deltaTime * RotatingSpeed);
     }
+
+    protected virtual void OnTriggerStay()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(ChangedScale,ChangedScale,ChangedScale), scaleSpeed * Time.deltaTime);
+    }
+
 }
